c2758859c230f13ce2474621b8903758dc2aa2a6
aiserban <aiserban@outlook.com>
2019-01-25 22:50:21 +0200

    Modified to directly get titles

--- AnalyzR/AnalyzR.py
+++ AnalyzR/AnalyzR.py
@@ -5,24 +5,19 @@ import re
 root_url = 'https://old.reddit.com/r/Romania/new/'
 
 soup = BeautifulSoup(requests.get(root_url, headers={'User-Agent': 'TestCrawler by NoSkillz05'}).text, 'html.parser')
-thread_regex = re.compile('https:\/\/old.reddit.com\/r\/Romania\/comments\/')
-load_more_regex = re.compile('^https:\/\/old.reddit.com\/r\/Romania\/new\/\?count=.*after')
 
-links = [] # storing all links here
-last_link = None
-last_id = None
-load_more_link = None
+titles = [] # storing all links here
 
 
-while len(links) < 100:
-    anchors = soup.find_all('a')
-    for anchor in anchors:
-        href = anchor.get('href')
-        if href is not None and href not in links and thread_regex.search(href):
-            links.append(href)
-        elif href is not None and load_more_regex.search(href):
-            load_more_link = href
+while len(titles) < 100:
+    threads = soup.find_all('a', {'class': 'title'})
+    for thread in threads:
+        title = thread.text
+        if title is not None and title not in titles:
+            titles.append(title)
+
+    load_more_link = soup.select_one('.next-button a').get('href')
 
     soup = BeautifulSoup(requests.get(load_more_link, headers={'User-Agent': 'TestCrawler by NoSkillz05'}).text, 'html.parser')
 
-print(links)
+print(titles)
